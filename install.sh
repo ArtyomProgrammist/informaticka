d9aa04d421cb9ce6c7690655ac56e5cececc838d
ArtyomProgrammist <115404868+ArtyomProgrammist@users.noreply.github.com>
2022-10-22 21:49:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 # Для установки import informaticka
-pip install git+git://github.com/ArtyomProgrammist/informaticka/
+pkg update
+pkg upgrade
+apt update
+apt upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pip install git+git://github.com/ArtyomProgrammist/informaticka
+pip install requests
